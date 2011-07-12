7165eb60337ea04ac774d2a0d3511f52eab8239e
James Whitehead II <jnwhiteh@gmail.com>
2011-07-12 17:25:00 +0100

    Remove load/loadstring

--- sandbox.lua
+++ sandbox.lua
@@ -3,26 +3,20 @@
 -- bash shell.  It provides a very simple sandbox that doesn't have any
 -- persistent state.  This is intentional.
 
-local modules = {
-	"bit",
-	"lpeg",
-	"md5",
-}
-
-pcall(require, "luarocks.require")
-
-for idx,mod in ipairs(modules) do
-	local succ, err = pcall(require, mod)
-end
-
 -- Grab the filename from the genv, so we have it available
 local filename = arg[1]
-local pluto_filename = arg[2]
+local session = arg[2]
+local luarocks = require("luarocks.require")
+local pluto = require("pluto")
 
 -- Save what we need to have access to in order to run
 local genv = getfenv(0)
-local io = genv.io
-local os = genv.os
+local os = {
+	date = genv.os.date,
+	time = genv.os.time,
+	difftime = genv.os.difftime,
+	clock = genv.os.clock,
+}
 local string = genv.string 
 local table = genv.table
 local type = genv.type
@@ -35,10 +29,12 @@ local pcall = genv.pcall
 local tostring = genv.tostring
 local pairs = genv.pairs
 local error = genv.error
-local pluto = select(2, pcall(require, "pluto"))
-local open = io.open
+local stdout = genv.io.stdout
+local open = genv.io.open
 local select = genv.select
-local getinfo = genv.debug.getinfo
+local rawset = genv.rawset
+local xpcall = genv.xpcall
+local traceback = genv.debug.traceback
 
 -- Make a copy of the true global environment
 local penv = {}
@@ -47,7 +43,8 @@ setmetatable(penv, getmetatable(genv))
 
 -- Clear the true global environment so we can build it from scratch
 for k,v in pairs(genv) do genv[k] = nil end
-setmetatable(genv, {__metatable={}})
+local genv_mt = {__metatable = {}}
+setmetatable(genv, genv_mt)
 
 -- This function allows you to expose global variables, as well as namespace functions
 -- i.e. it accepts keys such as "tostring", as well as "string.format".
@@ -79,13 +76,11 @@ expose{
 	"assert",			
 	"collectgarbage",		-- Should be safe in our throwaway environment, due to ulimit
 	"error",
-	"_G",					-- No reason this can't be exposed
-    "gcinfo",
 	"getfenv",				-- We should be fine with this, since they can't outside of it
 	"getmetatable",
 	"ipairs",
-	"load",
-	"loadstring",
+	--"load",               -- Corsix broke this one
+	--"loadstring",         -- Corsix broke this one
 	"next",
 	"pairs",
 	"pcall",
@@ -106,8 +101,6 @@ expose{
 	"os.date",
 	"os.difftime",
 	"os.time",
-	"os.setlocale",
-	"newproxy",				-- Until Shirik finds a way to break it
 }
 
 -- Export the strings,math and table libraries
@@ -116,13 +109,9 @@ for k,v in pairs(penv.string) do table.insert(libs, "string."..k) end
 for k,v in pairs(penv.math) do table.insert(libs, "math."..k) end
 for k,v in pairs(penv.table) do table.insert(libs, "table."..k) end
 for k,v in pairs(penv.coroutine) do table.insert(libs, "coroutine."..k) end
-for idx,mod in ipairs(modules) do
-	if penv[mod] then
-		for k,v in pairs(penv[mod]) do table.insert(libs, mod .. "." .. k) end
-	end
-end
-
 expose(libs)
+
+genv._G = genv
 
 -- Add the following lua-space split/join/concat/trim functions
 
@@ -176,165 +165,138 @@ genv.string.join = genv.strjoin
 genv.string.split = genv.strsplit
 genv.string.trim = genv.strtrim
 
--- Pretty print function
-
-do
-	local create_table_output
-	local get_value
-	local get_result
-
--- Serializes a table into a string
--- Code by: Cide of CTMod
-local get_value; -- Needs to be accessed by create_table_output
-local tbl_cache, tbl_num = { }, 0;
-function create_table_output(tbl, no_cache)
-	if ( tbl_cache[tbl] and not no_cache ) then
-		return "<table: "..tbl_cache[tbl]..">";
-	end
-
-	local address = tbl_cache[tbl];
-	if ( not address ) then
-		tbl_num = tbl_num + 1;
-		address = "#"..tbl_num;
-		tbl_cache[tbl] = address;
-	end
-
-	local msg = "{ ";
-	local num_ipairs = 0;
-	for key, value in ipairs(tbl) do
-		num_ipairs = key;
-		msg = msg .. get_value(value) .. ", ";
-	end
-	for key, value in pairs(tbl) do
-		if ( not tonumber(key) or math.floor(key) ~= key or ( key < 1 or key > num_ipairs ) ) then
-			msg = msg .. "["..get_value(key).."] = " .. get_value(value) .. ", ";
-		end
-	end
-	if ( string.match(msg, ", $") ) then
-		msg = string.sub(msg, 1, -3);
-	end
-	msg = msg .. " } ("..address..")";
-	return msg;
-end
-
-function get_result(...)
-	local t = { ... };
-	t.num = select('#', ...) - 1;
-	return t;
-end
-
--- Takes care of "serializing" a value
-function get_value(value, no_cache)
-	local vt = type(value);
-	if ( vt == "string" ) then
-		return "\""..value:gsub("\\", "\\\\"):gsub("\"", "\\\"").."\"";
-	elseif ( vt == "table" ) then
-		return create_table_output(value, no_cache);
-    elseif ( vt == "function" ) then
-        local info = getinfo(value, "n")
-        if info and info.name then
-            return string.format("function: '%s'", info.name)
-        else
-            return tostring(value)
-        end
-	else
-		return tostring(value);
-	end
-end
-
-	genv.prettyprint = function(...)
-		for i=1,select("#", ...) do
-			genv.print(get_value(select(i, ...)))
-		end
-	end
-
-	genv.pp = genv.prettyprint
-
-end
-
--- Custom private print function, will ensure the given 
--- prefix is printed before any other output
-local prefixSent = false
-local function output(prefix, text)
-	if not prefixSent then
-		io.stdout:write(prefix .. ":")
-		prefixSent = true
-	end
-
-	if text then
-		io.stdout:write(text)
-	end
-end
-
--- Add the custom print function
-local addComma = false
-function genv.print(...)
-	local max = select("#", ...)
-	for i=1,max do
-		if addComma then
-			io.stdout:write(", ")
-		else
-			addComma = true
-		end
-
-		local item = select(i, ...)
-		output("OUT", tostring(tostring(item)))
-	end
-end
-
 -- Let's properly sandbox the string metatable
 getmetatable("").__metatable = {}
 
-local file = io.open(filename, "r")
-if type(file) ~= "userdata" then
-	output("ERR", "Unexpected error running sandboxed code: file error")
-	os.exit(1)
-end
-
-local script,err = file:read("*all")
-if type(err) ~= "nil" then
-		output("ERR", "Unexpected error running sandbox code: read error")
-		os.exit(1)
-end
-
--- If the first non-whitespace token in an =, convert it to print
-
-if script:match("^%s*(%S)") == "=" then
-	script = script:gsub("^%s*(%S)", "return ")
-end
-
-local func,err = loadstring(script, "=luabot")
-local retfunc,reterr = loadstring("return " .. script, "=luabot")
-
--- Load any persistent state that has been sent to us
-if pluto_filename and pluto_filename:match("%S") then
-	local file = open(pluto_filename, "r")
-	if file then
-		local env = select(2, pcall(pluto.unpersist, {}, file:read("*all")))
-		if type(env) == "table" then
-			for k,v in pairs(env) do
-				genv[k] = v
-			end
+-- Utility functions that let us capture and release output
+local hijacknil = setmetatable({}, {__tostring=function() return "nil" end})
+local function capture(...)
+	local tbl = {}
+	for i=1,select("#", ...) do
+		local item = select(i, ...)
+		if type(item) == nil then
+			tbl[i] = hijacknil
+		else
+			tbl[i] = item
 		end
-		file:close()
+	end
+	
+	return tbl
+end
+
+local function release(tbl, s)
+	s = s or 1
+
+	if s > #tbl then
+		return
+	end
+
+	local item = tbl[s]
+	if item == hijacknil then
+		return nil, release(tbl, s + 1)
+	else
+		return item, release(tbl, s + 1)
 	end
 end
 
--- A whole bit of nastiness to signal the output of the script as either error
--- or actual result, and to attempt to 'print' the results of a script that
--- might not otherwise have a result
-if type(retfunc) ~= "function" then
+local no_persist = {}
+local c = 1
+local function flat_nopersist(tbl)
+	for k,v in pairs(tbl) do
+		if type(v) == "string" or type(v) == "number" then
+			--do nothing
+		elseif type(v) == "table" and not no_persist[v] then
+			no_persist[v] = 0
+			flat_nopersist(v)
+		else
+			no_persist[v] = c
+			c = c + 1
+		end
+	end
+end
+
+flat_nopersist(penv)
+flat_nopersist(genv)
+
+local function main()
+	-- Begin compiling and running the script.
+	-- Output a marker "RUN:" to show that we've actually began running the script
+	stdout:write("RUN:"); stdout:flush();
+
+	local file = open(filename, "r")
+	if type(file) ~= "userdata" then
+		print("ERR:Unexpected error running sandboxed code: file error")
+		os.exit(1)
+	end
+
+	local script,err = file:read("*all")
+	if type(err) ~= "nil" then
+		print("ERR:Unexpected error running sandbox code: read error")
+		os.exit(1)
+	end
+
+	local func,err = loadstring(script, "=weblua")
 	if type(func) ~= "function" then
 		-- We officially have nothing to run, bail out with the first error
-		output("ERR", err)
+		print("ERR:" .. tostring(err))
 		os.exit(1)
 	else
+		-- Create a capture table to get any new globals set by the script
+		local new_globals = {}
+		function genv_mt.__newindex(t,k,v)
+			rawset(new_globals, k, v)
+			rawset(t, k, v)
+		end
+
+		-- Load any persistent state that has been saved
+		local file = open("/tmp/webluasession-"..session, "r")
+		if file then
+			local env = select(2, pcall(pluto.unpersist, no_persist, file:read("*all")))
+			if type(env) == "table" then
+				for k,v in pairs(env) do
+					genv[k] = v
+				end
+			end
+			file:close()
+		end
+
 		-- Run this script, and hope it prints something
-		output("OUT")
-		genv.print(select(2, pcall(func)))
+
+		local handler = function(msg)
+			local stack = traceback()
+			stack = stack:gsub("%S+sputnik%-weblua[^\n]+%s+", "")
+			stack = stack:gsub("%s+%[C%]: in function 'xpcall'.+$", "")
+			return "<pre>" .. tostring(msg) .. "<br/>" .. stack .. "</pre>" 
+		end
+
+		local results = capture(xpcall(func, handler))
+		if results[1] then
+			-- pcall ran successfully, output any results
+
+			-- For everything in new_globals, update those values
+			for k,v in pairs(new_globals) do
+				rawset(new_globals, k, genv[k])
+			end
+
+			-- Save the state out to a session file
+			local file = open("/tmp/webluasession-"..session, "w+")
+			if file then
+				local buf = select(2, pcall(pluto.persist, no_persist, new_globals))
+				if type(buf) == "string" and #buf < (500 * 1024) then
+					file:write(buf)
+				end
+				file:close()
+			end
+
+			stdout:flush()
+			print(select(2, release(results)))
+			stdout:write(":END")
+		else
+			print(tostring(results[2]))
+			stdout:write(":ERR")
+		end
 	end
-else
-	output("OUT")
-	genv.print(select(2, pcall(retfunc)))
-	
 end
+
+return main()
+
